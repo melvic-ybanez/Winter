13ab783be38fa797ed9f61b0827d8111adad6b41
Melvic <melvic.ybanez2014@gmail.com>
2015-08-20 20:21:05 +0800

    add menu items to preferences menu

--- src/main/java/winter/views/menus/PreferencesMenu.java
+++ src/main/java/winter/views/menus/PreferencesMenu.java
@@ -1,12 +1,26 @@
 package winter.views.menus;
 
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 /**
  * Created by ybamelcash on 6/21/2015.
  */
 public class PreferencesMenu extends Menu {
+    private MenuItem generalItem;
+    private MenuItem fontItem;
+    private MenuItem syntaxItem;
+
     public PreferencesMenu() {
         super("Preferences");
+        init();
+    }
+
+    private void init() {
+        generalItem = new MenuItem("General...");
+        fontItem = new MenuItem("Font...");
+        syntaxItem = new MenuItem("Language Syntax...");
+
+        getItems().addAll(generalItem, fontItem, syntaxItem);
     }
 }
